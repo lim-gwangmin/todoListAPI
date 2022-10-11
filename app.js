fb1d148f491c905f304c5ac60e5079c303009812
임광민 <gm.lim@codexbridge.com>
2022-10-11 17:57:53 +0900

    todolist create api 작업중

--- app.js
+++ app.js
@@ -18,30 +18,40 @@ mongoose.connect(url)
   const TodoListSchema = new mongoose.Schema({
    name: {
      text: String,
-     edit: Boolean,
-     done: Boolean,
+   //   edit: Boolean,
+   //   done: Boolean,
    },
  });
-
 const todoList = mongoose.model("todoLists", TodoListSchema);
 
-function test() {
-   todoList.find((err, todo) => {
-      if (err) {
-        console.log(err);
-      } else {
-        mongoose.connection.close();
-        todo.forEach( item => {
-          console.log(item);
-        });
-      }
-    });
-}
+app.get('/', async (request, response) => { 
+   try { 
+      var result = await todoList.find().exec(); 
+      response.send(result); 
+   } catch (error) { 
+      response.status(500).send(error); 
+   } 
+});
 
-app.get("/", (req, res) => {
-   test();
-   res.send("Hello Word");
+app.post('/create', async (request, response) => { 
+   try { 
+      var createTodo = new todoList(request.body); 
+      // var result = await createTodo.save(); 
+      response.send(createTodo); 
+      // response.send(result); 
+   } catch (error) {
+      response.status(500).send(error); 
+   } 
 });
 
 
 
+app.delete('/delete:id', async (request, response) => { 
+   console.log(request)
+   try { 
+      var result = await todoList.deleteOne({ _id: request.params.id }).exec(); 
+      response.send(result); 
+   } catch (error) {
+       response.status(500).send(error); 
+      } 
+});
