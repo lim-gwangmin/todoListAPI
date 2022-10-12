3ab40cbb3566b4bc90fe01f58013e80fc5f5dc0a
임광민 <gm.lim@codexbridge.com>
2022-10-12 10:56:43 +0900

    /create API 추가

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const url = `mongodb+srv://root:xHtzS0YfTkoGNE7e@cluster0.0el4m.mongodb.net/Cluster0`;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 mongoose.connect(url)
   .then(() => {
@@ -15,12 +21,11 @@ mongoose.connect(url)
   });
 
 
-  const TodoListSchema = new mongoose.Schema({
-   name: {
-     text: String,
-   //   edit: Boolean,
-   //   done: Boolean,
-   },
+const TodoListSchema = new mongoose.Schema({
+     id: { type: Number, required: true, unique: true },
+     text: { type: String, required: true },
+     edit: { type: Boolean, default: false },
+     done: { type: Boolean, default: false }
  });
 const todoList = mongoose.model("todoLists", TodoListSchema);
 
@@ -34,22 +39,33 @@ app.get('/', async (request, response) => {
 });
 
 app.post('/create', async (request, response) => { 
+
+   const { text } = request.body; 
+
    try { 
-      var createTodo = new todoList(request.body); 
-      // var result = await createTodo.save(); 
-      response.send(createTodo); 
-      // response.send(result); 
+      if(!text) {
+         response.send({result:false, message:'텍스트가 입력되지 않았습니다.'});
+      };
+      var count = await todoList.find().exec(); 
+
+      var createTodo = new todoList({
+         id: count.length + 1,
+         text: text,
+         edit: false,
+         done: false,
+      }); 
+      var result = await createTodo.save(); 
+      response.send({result: true, resultData: result}); 
    } catch (error) {
       response.status(500).send(error); 
    } 
 });
 
 
-
 app.delete('/delete:id', async (request, response) => { 
-   console.log(request)
    try { 
-      var result = await todoList.deleteOne({ _id: request.params.id }).exec(); 
+      var result = await todoList.find().exec(); 
+      // var result = await todoList.deleteOne({ _id: request.params.id }).exec(); 
       response.send(result); 
    } catch (error) {
        response.status(500).send(error); 
